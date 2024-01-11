cf26e2c21d13f5ce3459418a862f86de647937ba
Dahyeon Han <52192706+ekgus9701@users.noreply.github.com>
2024-01-11 13:03:18 +0900

    한다현 / 20240115 / [리트코드] FirstBadVersion.cpp

--- ekgus9701/240115/lcd_FirstBadVersion.cpp
+++ ekgus9701/240115/lcd_FirstBadVersion.cpp
@@ -1,7 +1,7 @@
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
-class Solution {
+class Solution { 
 public:
     int firstBadVersion(int n) {
         //if(n==1) return 1;
